d721ac34920e04e21c8daa6e27a11bf2c0a53d6a
Alastair Antonio <alastair.antonio@gmail.com>
2023-05-05 23:29:32 -0700

    fixed the filter to look for the custom header

--- src/main/java/org/springframework/samples/petclinic/filter/CustomHeaderFilter.java
+++ src/main/java/org/springframework/samples/petclinic/filter/CustomHeaderFilter.java
@@ -3,38 +3,19 @@ package org.springframework.samples.petclinic.filter;
 import io.opentelemetry.api.trace.Span;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CustomHeaderFilter implements Filter {
-
-    private final List<String> headerNames;
-
-    public CustomHeaderFilter() {
-        headerNames = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("headerCapture.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                headerNames.add(line.trim());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpRequest = (HttpServletRequest) request;
-            for (String headerName : headerNames) {
-                String headerValue = httpRequest.getHeader(headerName);
-                if (headerValue != null) {
-                    parseCustomHeader(headerValue);
-                }
+            String headerValue = httpRequest.getHeader("x-splunk-test");
+
+            if (headerValue != null) {
+                parseCustomHeader(headerValue);
             }
         }
         chain.doFilter(request, response);
@@ -50,4 +31,5 @@ public class CustomHeaderFilter implements Filter {
             }
         }
     }
+
 }
